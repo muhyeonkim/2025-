b91ebdd5fabcecd5a97ec6411bb346fd803e9778
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-01-08 02:11:10 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -6,15 +6,15 @@ int main() {
 	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int sum=i, part=i;
-		while (part) {
-			sum += part % 10;
-			part /= 10;
+		int sum = i, tmp = i;
+		while (tmp != 0) {
+			sum += tmp % 10;
+			tmp = tmp / 10;
 		}
 		if (sum == n) {
 			cout << i;
 			return 0;
 		}
 	}
-	cout << "0";
+	cout << 0;
 }
