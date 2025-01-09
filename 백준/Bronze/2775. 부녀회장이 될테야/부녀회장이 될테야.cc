dcec267218325283a7cc70ee1a29dbf9344d1e73
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-01-09 15:10:01 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -11,9 +11,7 @@ int main() {
 	}
 	for (int i = 1; i <= 14; i++) {
 		for (int j = 1; j <= 14; j++) {
-			for (int k = 1; k <= j; k++) {
-				arr[i][j] += arr[i-1][k];
-			}
+			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
 		}
 	}
 
