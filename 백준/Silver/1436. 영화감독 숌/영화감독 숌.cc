621bc774b2ae62d34eac5a8db20f184c5d87fef1
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-01-12 19:26:18 +0900

    [Silver V] Title: 영화감독 숌, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,40 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-/*
-숫자를 하나 씩 증가시킨다.
-666이 있는지 확인한다.->?
-n번 째 숫자를 출력한다.
-
-  666이 있는지 확인한다
-->숫자를 분리시킨다.
-->분리시킨 숫자들을 배열에 담는다.
-->3번 연속인지 확인한다.
-*/
-
-bool check(int n) {
-	vector<int>tmp;
-	while (n != 0) {
-		tmp.push_back(n % 10);
-		n /= 10;
-	}
-
-	for (int i = 2; i < tmp.size(); i++) {
-		if (tmp[i] == 6 && tmp[i - 1] == 6 && tmp[i - 2] == 6) {
-			return true;
-		}
-	}
-	return false;
-}
-
 int main() {
-	int n, num = 1, cnt = 0;
+	int n, num = 0;
 	cin >> n;
-	while (1) {
-		if (check(num)) cnt++;
-		if (cnt == n) break;
+	while (n!=0) {
 		num++;
+		string tmp = to_string(num);
+		if (tmp.find("666") != string::npos) n--;
 	}
 	cout << num;
 }
