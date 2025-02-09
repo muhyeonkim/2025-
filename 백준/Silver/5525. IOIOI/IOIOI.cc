7a99df3622530e0837adea49cb75b7fa17a84ec6
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-02-09 20:16:03 +0900

    [Silver I] Title: IOIOI, Time: 36 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+/*
+1. pn은 I+OI*n이다. 
+2. 현재 위치와 그다음이 OI인지 확인한다.OI가 맞으면 tmp에 추가, 아니면 tmp를 초기화 한다.
+3. p와 tmp가 동일하면 cnt를 증가한다.
+
+*/
 int main() {
 	int n, m, cnt = 0;
-	string s, p = "I";
+	string s;
 	cin >> n >> m >> s;
-	for (int i = 0; i < n; i++) {
-		p += "OI";
-	}
 
-	for (int i = 0; i < m-p.length()+1; i++) {
-		string tmp = "";
-		for (int j = 0; j < p.length(); j++) {
-			tmp += s[i+j];
+	for (int i = 0; i < m; i++) {
+		int len = 0;
+		if (s[i] == 'O') continue;
+		else {
+			while (s[i + 1] == 'O' && s[i + 2] == 'I') {
+				len++;
+				if (len == n) {
+					cnt++;
+					len--;
+				}
+				i += 2;
+			}
 		}
-		if (tmp == p) cnt++;
 	}
 	cout << cnt;
 }
