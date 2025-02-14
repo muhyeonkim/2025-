f1f4be313740f508bbc54d7ea2fc49cecc6645db
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-02-14 18:38:04 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2108 ms, Memory: 57008 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,72 +1,73 @@
 #include <iostream>
-#include <queue>
 #include <map>
-
+#include <queue>
 using namespace std;
 
-void processOperations(int k) {
-    priority_queue<int> maxHeap; // 최대 힙
-    priority_queue<int, vector<int>, greater<int>> minHeap; // 최소 힙
-    map<int, int> countMap; // 각 숫자의 개수 저장
+/*
+삽입 할 때 3개에 다넣는다.
+삭제 할 때 1이면  map의 value감소 후 max_pq에서 삭제
+삭제 할 때 -1이면  map의 value감소 후 min_pq에서 삭제
+해당 key의 value가 0이면 다음 원소 삭제
 
-    while (k--) {
-        char op;
-        int num;
-        cin >> op >> num;
-
-        if (op == 'I') { // 삽입 연산
-            maxHeap.push(num);
-            minHeap.push(num);
-            countMap[num]++;
-        }
-        else if (op == 'D') { // 삭제 연산
-            if (countMap.empty()) continue; // 큐가 비어 있으면 무시
-
-            if (num == 1) { // 최댓값 삭제
-                while (!maxHeap.empty() && countMap[maxHeap.top()] == 0) {
-                    maxHeap.pop(); // 이미 삭제된 값 무시
-                }
-                if (!maxHeap.empty()) {
-                    countMap[maxHeap.top()]--;
-                    maxHeap.pop();
-                }
-            }
-            else { // 최솟값 삭제
-                while (!minHeap.empty() && countMap[minHeap.top()] == 0) {
-                    minHeap.pop();
-                }
-                if (!minHeap.empty()) {
-                    countMap[minHeap.top()]--;
-                    minHeap.pop();
-                }
-            }
-        }
-    }
-
-    // 최종 결과 확인
-    while (!maxHeap.empty() && countMap[maxHeap.top()] == 0) {
-        maxHeap.pop();
-    }
-    while (!minHeap.empty() && countMap[minHeap.top()] == 0) {
-        minHeap.pop();
-    }
-
-    if (maxHeap.empty() || minHeap.empty()) {
-        cout << "EMPTY\n";
-    } else {
-        cout << maxHeap.top() << " " << minHeap.top() << "\n";
-    }
-}
+현재 top의 value가 0이 아닐 때 까지 pop
+그 후 top의 value를 1 감소, 0이면 pop 아니면 그대로 두기
+*/
 
 int main() {
-    int T;
-    cin >> T; // 테스트 케이스 개수
+	int t;
+	cin >> t;
+	while (t--) {
+		map<int, int>m; // key, value
+		priority_queue<int>max_pq; // 내림 차순
+		priority_queue<int, vector<int>, greater<>>min_pq; // 오름 차순
+		int k;
+		cin >> k;
+		while (k--) {
+			char c;
+			int x;
+			cin >> c >> x;
+			if (c == 'I') {
+				max_pq.push(x);
+				min_pq.push(x);
+				m[x]++;
+			}
+			else { // c=='D'
+				if (m.empty() == 1) {
+					continue;
+				}
+				if (x == 1) { // 최대 
+					while (max_pq.empty() == 0 && m[max_pq.top()] == 0) {
+						max_pq.pop();
+					}
+					if (max_pq.empty() == 0) {
+						m[max_pq.top()]--;
+						max_pq.pop();
+					}
+				}
+				else {
+					while (min_pq.empty() == 0 && m[min_pq.top()] == 0) {
+						min_pq.pop();
+					}
+					if (min_pq.empty() == 0) {
+						m[min_pq.top()]--;
+						min_pq.pop();
+					}
+				}
+			}
+		}
+		while (max_pq.empty() == 0 && m[max_pq.top()] == 0) {
+			max_pq.pop();
+		}
 
-    while (T--) {
-        int k;
-        cin >> k; // 연산 개수
-        processOperations(k);
-    }
+		while (min_pq.empty() == 0 && m[min_pq.top()] == 0) {
+			min_pq.pop();
+		}
 
-    return 0;
+		if (max_pq.empty() == 1 || min_pq.empty() == 1) {
+			cout << "EMPTY" << "\n";
+		}
+		else {
+			cout << max_pq.top() << ' ' << min_pq.top() << "\n";
+		}
+	}
 }
