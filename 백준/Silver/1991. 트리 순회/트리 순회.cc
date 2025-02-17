15fbf2681b0adef2f5f7d0577d022adcbd5ac8f0
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-02-17 19:02:44 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+char tree[26][2];
+
+void travel1(int x) { // 전위
+	cout << char(x+'A');
+	if (tree[x][0] != '.') {
+		travel1(tree[x][0] - 'A');
+	}
+	if (tree[x][1] != '.') {
+		travel1(tree[x][1] - 'A');
+	}
+}
+
+void travel2(int x) { // 중위
+	if (tree[x][0] != '.') {
+		travel2(tree[x][0] - 'A');
+	}
+	cout << char(x + 'A');
+
+	if (tree[x][1] != '.') {
+		travel2(tree[x][1] - 'A');
+	}
+}
+
+void travel3(int x) { // 후위
+	if (tree[x][0] != '.') {
+		travel3(tree[x][0] - 'A');
+		cout << tree[x][0];
+	}
+
+	if (tree[x][1] != '.') {
+		travel3(tree[x][1] - 'A');
+		cout << tree[x][1];
+	}
+}
+int main() {
+	int  n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char first, second, third;
+		cin >> first >> second >> third;
+		
+		if (second == '.') {
+			tree[first - 'A'][0] = '.';
+		}
+		else {
+			tree[first - 'A'][0] = second;
+		}
+		if (third == '.') {
+			tree[first - 'A'][1] = '.';
+		}
+		else {
+			tree[first - 'A'][1] = third;
+		}
+	}
+	
+	travel1(0);
+	cout << "\n";
+	travel2(0);
+	cout << "\n";
+	travel3(0);
+	cout << 'A' << "\n";
+}
