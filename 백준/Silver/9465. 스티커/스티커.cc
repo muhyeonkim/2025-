7c5ff2e2eb49133d3cb6cc319b85fbc5eb19f57e
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-02-17 22:24:54 +0900

    [Silver I] Title: 스티커, Time: 256 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -2,32 +2,30 @@
 
 using namespace std;
 
-int stick[2][100000];
-int dp[3][100000];
+int stick[2][100001];
+int dp[3][100001];
 int main() {
 	int t;
 	cin >> t;
 	while (t--) {
 		int n;
 		cin >> n;
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			cin >> stick[0][i];
 		}
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			cin >> stick[1][i];
 		}
-		dp[0][0] = stick[0][0];
-		dp[1][0] = stick[1][0];
+		dp[0][1] = stick[0][1];
+		dp[1][1] = stick[1][1];
 		/*dp[0][1] = stick[1][0] + stick[0][1];
 		dp[1][1] = stick[0][0] + stick[1][1];
 		dp[2][1] = max(dp[0][0], dp[1][0]);*/
 		
-		for (int i = 1; i < n; i++) {
-			dp[0][i] = max(dp[1][i - 1] + stick[0][i], dp[2][i - 1] + stick[0][i]); // 위의 스티커를 고르는 경우
-			dp[1][i] = max(dp[0][i - 1] + stick[1][i], dp[2][i - 1] + stick[1][i]); // 아래 스티커를 고르는 경우
-			dp[2][i] = max(dp[0][i - 1], dp[1][i - 1]);//아무것도 안 고르는 경우
-			//cout << dp[0][i] << ' ' << dp[1][i] << "\n";
+		for (int i = 2; i <= n; i++) {
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2])+ stick[0][i]; // 위의 스티커를 고르는 경우
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2])+ stick[1][i]; // 아래 스티커를 고르는 경우
 		}
-		cout << max(dp[0][n - 1], dp[1][n - 1]) << "\n";
+		cout << max(dp[0][n], dp[1][n]) << "\n";
 	}
 }
