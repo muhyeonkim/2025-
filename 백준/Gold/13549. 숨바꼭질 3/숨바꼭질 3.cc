6e9ca7401fe112ab09ad158777f85b9a64eb18cd
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-02-19 15:56:53 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <deque>
-#include <vector>
+#include <queue>
+
 using namespace std;
 
 vector<int>map(100001, 100001);
@@ -15,33 +15,31 @@ bool isOutofLine(int x) {
 }
 
 void bfs(int x) {
-	deque<int>dq;//좌표, 시간
+	queue<int>q;//좌표, 시간
 	map[x] = 0;
-	dq.push_back(x);
-	while (k!=dq.front()) {
-		int nx = dq.front();
+	q.push(x);
+	while (k!=q.front()) {
+		int nx = q.front();
 		
-		dq.pop_front();
-		if (isOutofLine(nx * 2) && nx != 0) { //*2
-			if (map[nx * 2] > map[nx]) {
-				map[nx * 2] = map[nx];
-				dq.push_front(nx * 2);
-				
-			}
-		}
+		q.pop();
 		if (isOutofLine(nx - 1)) {//-1
 			if (map[nx - 1] > map[nx] + 1) {
 				map[nx - 1] = map[nx] + 1;
-				dq.push_back(nx - 1);
+				q.push(nx - 1);
 			}
 		}
 		if (isOutofLine(nx + 1)) { // +1
 			if (map[nx + 1] > map[nx] + 1) {
 				map[nx + 1] = map[nx] + 1;
-				dq.push_back(nx + 1);
+				q.push(nx + 1);
 			}
 		}
-		
+		if (isOutofLine(nx * 2) && nx != 0) { //*2
+			if (map[nx * 2] > map[nx]) {
+				map[nx * 2] = map[nx];
+				q.push(nx * 2);
+			}
+		}
 	}
 }
 
