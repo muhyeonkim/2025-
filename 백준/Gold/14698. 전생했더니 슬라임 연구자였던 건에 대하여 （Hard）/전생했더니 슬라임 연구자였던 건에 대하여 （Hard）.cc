984c12463d84285024b7a9a2cdc986635111c294
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-07 16:55:01 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 164 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.cc
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.cc
@@ -1,47 +1,43 @@
 #include <iostream>
 #include <queue>
+/*
+최소 값끼리 계속 곱하기
+long long int
+*/
 
 using namespace std;
 
-const long long MOD = 1000000007;
-
 int main() {
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-    int t;
-    cin >> t;
-    
-    while (t--) {
-        int n;
-        cin >> n;
 
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        
-        for (int i = 0; i < n; i++) {
-            long long x;
-            cin >> x;
-            pq.push(x);
-        }
-
-        if (n == 1) { // 슬라임이 1마리면 비용 1 출력
-            cout << 1 << "\n";
-            continue;
-        }
-
-        long long cost = 1; // 결과값 (곱셈이므로 초기값 1)
-        
-        while (pq.size() > 1) {
-            long long first = pq.top(); pq.pop();
-            long long second = pq.top(); pq.pop();
-
-            long long energy = (first * second) % MOD; // 곱할 때마다 MOD 연산
-            cost = (cost * energy) % MOD; // 총 비용도 MOD 연산
-
-            pq.push(first * second); // 새로운 슬라임 추가
-        }
-
-        cout << cost << "\n";
-    }
-    
-    return 0;
+	int t;
+	cin >> t;
+	while (t--) {
+		long long int sum = 1;
+		priority_queue<long long int, vector<long long int>, greater<>>pq;
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			long long int x;
+			cin >> x;
+			pq.push(x);
+		}
+		if (n == 1) { 
+			cout << 1 << "\n"; 
+			continue;
+		}
+	
+		while (pq.size() > 1) {
+			long long int first = pq.top();
+			pq.pop();
+			long long int second = pq.top();
+			pq.pop();
+			long long energy= (first * second) % 1000000007;
+			sum = (sum*energy) %1000000007;
+			//cout << first * second << ' ';
+			pq.push(first * second);
+		}
+		cout << sum<<"\n";
+	}
 }
