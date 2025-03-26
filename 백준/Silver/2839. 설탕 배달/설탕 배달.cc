e6c086714ba4982582e4a520a1f0cc059744b707
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-26 13:53:07 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,17 +1,20 @@
 #include <iostream>
 
 using namespace std;
-/*
-n을 3,5로 나눔, 가장 적은 봉지 수는? 
-3을 계속 빼다가 5로 나눌 수 있으면 그때 몫을 cnt에 더하기
-*/
+
 int main() {
 	int n, cnt = 0;
 	cin >> n;
-	while (n % 5 != 0) {
-		n -= 3;
-		cnt++;
+	while (n > 0) {
+		if (n % 5 == 0) {
+			cnt += n / 5;
+			break;
+		}
+		else {
+			n -= 3;
+			cnt++;
+		}
 	}
 	if (n < 0) cout << -1;
-	else cout << cnt + n / 5;
+	else cout << cnt;
 }
