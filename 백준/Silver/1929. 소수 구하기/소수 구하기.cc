254f88c0258c5974ff272147ab5af162b52e6f67
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-26 14:13:20 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,25 +1,27 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 
 using namespace std;
+
+int prime[1000001];
+
 int main() {
-	int m, n;
-	cin >> m >> n;
-	vector<int>v;
-	for (int i = 0; i <= n; i++) {
-		v.push_back(i);
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 2; i <= m; i++) {
+		prime[i] = i;
 	}
 
-	for (int i = 2; i <= n; i++) {
-		if (v[i] == 0) continue;
-		for (int j = i + i; j <= n; j += i) {
-			v[j] = 0;
+	for (int i = 2; i <= m; i++) {
+		if (prime[i] == 0) continue;
+		for (int j = i * 2; j <= m; j += i) {
+			prime[j] = 0;
 		}
 	}
-
-	for (int i = m; i <= n; i++) {
-		if (v[i] != 0 && v[i] >= 2) cout << v[i] << "\n";
+	for (int i = n; i <= m; i++) {
+		if (prime[i] != 0) cout << prime[i] << "\n";
 	}
-
 }
