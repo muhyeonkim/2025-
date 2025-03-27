b5c47c4294bceb135a722a92ffc4afc2dbfd65c0
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-27 16:56:10 +0900

    [Silver IV] Title: 듣보잡, Time: 44 ms, Memory: 7580 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -6,26 +6,29 @@
 using namespace std;
 
 int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
 	int n, m;
-	set<string>st;
-	vector<string>v;
+	set<string>name;
+	vector<string>res;
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		string s;
 		cin >> s;
-		st.insert(s);
+		name.insert(s);
 	}
 
 	for (int i = 0; i < m; i++) {
 		string s;
 		cin >> s;
-		if (st.find(s) != st.end()) {
-			v.push_back(s);
+		if (name.find(s) != name.end()) { //듣도 못한 사람 명단에 있을 때
+			res.push_back(s);
 		}
 	}
-	sort(v.begin(), v.end());
-	cout << v.size()<<"\n";
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << "\n";
+	sort(res.begin(), res.end());
+	cout << res.size() << "\n";
+	for (auto a : res) {
+		cout << a << "\n";
 	}
 }
