55564f651a8c4246f700e411c774f517cd5df7a6
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-28 16:11:19 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,28 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-/*
-오름 차순으로 하면 0번 원소를 1번, 마지막 원소를 n번 더하게 된다.
-따라서 오름 차순이 아닌 내림차순으로 정렬해서 더한다.
-혹은 오름 차순으로 정렬 후 역방향으로 더한다.
-*/
+
 using namespace std;
 
 int main() {
-	int n, sum = 0;
+	long long int n, sum = 0;
 	vector<int>v;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int p;
-		cin >> p;
-		v.push_back(p);
+		int x;
+		cin >> x;
+		v.push_back(x);
 	}
-
-	sort(v.begin(), v.end(), greater<>());
-	
-
+	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
+		for (int j = n - i - 1; j >= 0; j--) {
 			sum += v[j];
 		}
 	}
