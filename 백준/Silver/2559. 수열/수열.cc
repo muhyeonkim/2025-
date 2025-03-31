1b039909e1ec900248b21b2137afc9546da1c096
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-03-31 16:00:24 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -16,17 +16,14 @@ int main() {
 		cin >> x;
 		v.push_back(x);
 	}
-	int left = 0, sum = 0, max_temp = INT_MIN, length = 0;
-	for (int right = 0; right < n; right++) {
-		sum += v[right];
-		length++;
-		if (length == k) {
-			//cout << sum << "\n";
-			max_temp = max(max_temp, sum);
-			sum -= v[left];
-			left++;
-			length--;
-		}
+	int sum = 0, max_temp = INT_MIN;
+	for (int i = 0; i < k; i++) {
+		sum += v[i];
+	}
+	max_temp = sum;
+	for (int right = k; right < n; right++) {
+		sum += v[right] - v[right - k];
+		max_temp = max(max_temp, sum);
 	}
 	cout << max_temp;
 }
