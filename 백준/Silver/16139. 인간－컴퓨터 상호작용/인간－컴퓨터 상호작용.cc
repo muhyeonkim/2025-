0fd13415458ad1e73b2e0fcdbc7dea4b33399441
muhyeonkim <114920847+muhyeonkim@users.noreply.github.com>
2025-04-07 17:22:52 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 22336 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/*
+문자열에서 특정 알파벳이 몇 번 나타나는지 알아봐서
+자주 나타나는 알파벳이 중지나 검지 위치에 오는 알파벳인지 확인
+l번째 문자부터 r번째 문자 사이에 a가 나타나는 횟수 출력
+누적 합 : s[i]는 0번부터 i-1번째 원소까지의 합
+*/
+
+using namespace std;
+
+int arr[200001][26];
+
+int main() {
+	string s;
+	int q;
+	cin >> s >> q;
+	for (int i = 0; i < s.length(); i++) {
+		for (int j = 0; j < 26; j++) {
+			arr[i + 1][j] = arr[i][j];
+		}
+		arr[i + 1][s[i] - 'a']++;
+	}
+
+	for (int i = 0; i < q; i++) {
+		char a;
+		int l, r;
+		cin >> a >> l >> r;
+		cout << arr[r+1][a - 'a'] - arr[l][a - 'a'] << "\n";
+	}
+
+}
